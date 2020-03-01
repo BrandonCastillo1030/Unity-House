5c99db80db763b3693e3c9afe20d09b1b6c6a34c
Brandon Castillo <brandoncastillo1030@gmail.com>
2020-03-01 16:54:57 -0500

    added House

---
+++ House/Assets/Scripts/Door.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    public GameObject Cube;
+    int doorX = 1;
+    int doorY = 1;
+    int doorZ = 6;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // It took me awhile, but now I understand how nested loops work a bit more
+        for (int i = 0; i < 2; i++)
+        {
+            for (int j = 0; j < 2; j++)
+            {
+                Instantiate(Cube, new Vector3(doorX, doorY, doorZ), Quaternion.identity);
+                doorX += 1;
+            }
+            doorX = 1;
+            doorY += 1;
+        }
+    }
+}
